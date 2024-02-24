bf0edf964ef1c224518dd9ed7801007500e12dba
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-24 06:22:33 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Youtube Thumbnail Download | Download YouTube Thumbnails for Free",
+  title: "Youtube Thumbnail Download",
   description:
     "Youtube Thumbnail Download | Download YouTube Thumbnails for Free",
   icon: "/logo.jpg",
