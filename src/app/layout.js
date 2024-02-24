5d1e390be3f6ce5771431f165f573cb3f1925cd5
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-24 06:41:15 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Youtube Thumbnail Download",
   description:
-    "Youtube Thumbnail Download | Download YouTube Thumbnails for Free",
+    "Youtube Thumbnail Download | Download YouTube Thumbnails for Free, Youtube Thumbnail Image",
   icon: "/logo.jpg",
 };
 
