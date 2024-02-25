f2738726d14102d2592ba8d097b3b2d7af3be2fb
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-24 22:39:03 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,13 +36,7 @@ export default function RootLayout({ children }) {
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8683978034260199"
      crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VZQRFRCY3D"></script>
-<script>
- window && window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
 
-  gtag('config', 'G-VZQRFRCY3D');
-</script>
       <body className={inter.className}>
         <ToastContainer
           position="top-center"
