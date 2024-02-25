59a446289c93dc449e7478a55d62374e6f3c900e
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-24 22:57:58 -0800

    Update gtag.js

--- src/app/gtag.js
+++ src/app/gtag.js
@@ -3,6 +3,7 @@ import React from 'react'
 const gtag = () => {
   return (
     <>
+    <h1>fghfgfgh</h1>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VZQRFRCY3D"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
