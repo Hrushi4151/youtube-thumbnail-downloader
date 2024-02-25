8015e863a9b2c1f625ea042e91c4eef6e1679a75
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-24 20:12:52 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,15 +11,17 @@ export const metadata = {
   title: "Youtube Thumbnail Download",
   description:
     "Youtube Thumbnail Download | Download YouTube Thumbnails for Free, Youtube Thumbnail Image",
-  icon: "/logo.jpg",
+  icon: "/favicon.ico",
 };
 
 export default function RootLayout({ children }) {
   let schemadata = {
-    "@context": "https://schema.org/",
-    "@type": "Website",
-    name: "Youtube Thumbnail Download",
-    url: "https://www.youtube-thumbnail-download-er.vercel.app",
+    "@context":"https://schema.org",
+      "@type":"WebSite",
+      "name":"YouTube Thumbnail Download",
+      "alternateName":"YouTube Thumbnail Downloader",
+      "url":"https://youtube-thumbnail-download-er.vercel.app/",
+      "description":"Youtube Thumbnail Download | Download YouTube Thumbnails for Free, Youtube Thumbnail Image",
   };
   return (
     <html lang="en">
@@ -30,16 +32,7 @@ export default function RootLayout({ children }) {
         sizes="<generated>"
       />
       <meta name="google-adsense-account" content="ca-pub-8683978034260199"></meta>
-      <script type="application/ld+json">
-    {
-      "@context":"https://schema.org",
-      "@type":"WebSite",
-      "name":"YouTube Thumbnail Download",
-      "alternateName":"YouTube Thumbnail Downloader",
-      "url":"https://youtube-thumbnail-download-er.vercel.app/",
-      "description":"Youtube Thumbnail Download | Download YouTube Thumbnails for Free, Youtube Thumbnail Image",
-      }
-  </script>
+      <script type="application/ld+json"/>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8683978034260199"
      crossorigin="anonymous"></script>
       <body className={inter.className}>
