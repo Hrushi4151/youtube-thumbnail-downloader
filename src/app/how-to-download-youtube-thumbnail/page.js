403d65e104f43520872ff24cbdaee4c2d76e9d0f
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-02-27 08:48:24 -0800

    Add files via upload

---
+++ src/app/how-to-download-youtube-thumbnail/page.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div class="-[90vw] md:w-[80vw] mx-auto my-5">
+        <h1 class=" my-4 font-bold text-2xl">How to download Thumbnail from Youtube online 2024</h1>
+        <p className="text-lg">
+          <i>
+            How to download Thumbnail from Youtube? This is a question
+            that many Youtube users are interested in, in this article I will
+            show you how to use YouTube Video Downloader to download Thumbnail from
+            Youtube online on PC, iPhone, Android.
+          </i>
+        </p>
+        <p className="text-lg">
+          YouTube Video Downloader is an Youtube downloader, allow to download Shorts,
+          videos, Insta reels, story and IGTV from Youtube in the highest
+          quality. Just paste the Youtube link into the input box on YouTube Video Downloader
+          and you can download any Youtube post content for free.
+        </p>
+        <h2 className="text-xl font-semibold my-2 mt-5">Step 1: Copy Youtube link</h2>
+        <p className="text-lg">
+          Find the content you want to download and click on the icon above the
+          post and then continue pressing the <strong>Copy Link</strong> option.
+        </p>
+        <p className="text-lg">
+          <img
+            className="block ml-auto mr-auto"
+            src="/how-to-web-step-1-min.webp"
+            width="800"
+            height="527"
+            alt="YouTube Video Downloader"
+          />
+        </p>
+        <h2 className="text-xl font-semibold my-2 mt-5">Step 2: Paste the copied Youtube link into YouTube Video Downloader</h2>
+        <p className="text-lg">
+          - Open a browser and go to the website:{" "}
+          <a target="_blank" href="https://youtube-thumbnai-download-er.app/">
+            YouTube Video Downloader
+          </a>
+        </p>
+        <p className="text-lg">
+          - Paste the copied Youtube link into the input box and press the{" "}
+          <strong>Download</strong> button.
+        </p>
+        <p className="text-lg">
+          <img
+            className="block ml-auto mr-auto"
+            src="/how-to-web-step-2-min.webp"
+            width="800"
+            height="411"
+            alt="YouTube Video Downloader"
+          />
+        </p>
+        <h2 className="text-xl font-semibold my-2 mt-5">Step 3: Download Shorts or videos to your device</h2>
+        <p className="text-lg">
+          - <strong>Photo</strong> and <strong>Video</strong> you want to
+          download will appear, click <strong>Download Photo</strong> or{" "}
+          <strong>Download Video</strong> button inside under a photo or video,
+          then the file will be saved to your device.
+        </p>
+        <p className="text-lg">
+          <img
+           className="block ml-auto mr-auto"
+            src="/how-to-web-step-3-min.webp"
+            width="800"
+            height="418"
+            alt="YouTube Video Downloader"
+          />
+        </p>
+        <p className="text-lg">
+          <b>Note</b>
+        </p>
+        
+        <p className="text-lg">
+          During use, if you encounter an error, please contact us for support:{" "}
+          <a href="mailto:instasaver@gmail.com">contact:instasaver@gmail.com</a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default page;
